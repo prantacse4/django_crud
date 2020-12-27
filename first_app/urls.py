f60859fa635a03206373254a9cef50e4c9c0da88
prantacse4 <pranta.cse4.bu@gmail.com>
2020-12-27 19:19:42 +0600

    Final Commit

--- first_app/urls.py
+++ first_app/urls.py
@@ -6,10 +6,10 @@ from first_app import views
 app_name = "first_app"
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('',views.home, name="home"),
     path('student_form/',views.student_form, name="student_form"),
-    path('student_info/',views.student_info, name="student_info"),
-    path('update_info/',views.update_info, name="update_info"),
+    path('student_info/<int:student_id>',views.student_info, name="student_info"),
+    path('update_info/<int:student_id>',views.update_info, name="update_info"),
+    path('delete_info/<int:student_id>', views.delete_info, name="delete_info"),
 
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from first_app.models import StudentModel
+from first_app import forms
 
 # Create your views here.
 
@@ -9,16 +10,45 @@ def home(request):
     return render(request, 'first_app/index.html', context = diction)
 
 
-def student_info(request):
-    diction={}
-    return render(request, 'first_app/student_info.html', context = diction)
-
-
 def student_form(request):
-    diction={}
+    new_form = forms.StudentForm()
+
+    if  request.method == "POST":
+        new_form = forms.StudentForm(request.POST)
+
+        if new_form.is_valid():
+            new_form.save(commit=True)
+            return home(request)
+
+    diction={'title':"StudentForm", "student_form":new_form}
     return render(request, 'first_app/student_form.html', context = diction)
 
 
-def update_info(request):
-    diction={}
+
+def student_info(request, student_id):
+    student_info = StudentModel.objects.get(pk=student_id)
+    diction={'student_info':student_info}
+    return render(request, 'first_app/student_info.html', context = diction)
+
+
+
+def update_info(request, student_id):
+    student_info = StudentModel.objects.get(pk = student_id)
+    form = forms.StudentForm(instance = student_info)
+
+    if request.method == "POST":
+        form = forms.StudentForm(request.POST, instance=student_info)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return home(request)
+    diction={'student_form':form}
     return render(request, 'first_app/update_info.html', context = diction)
+
+
+def delete_info(request, student_id):
+    delete_info = StudentModel.objects.get(pk=student_id).delete()
+    mgs = "Deleted Successfully"
+    diction = {'mgs':mgs}
+    return render(request, 'first_app/index.html', context = diction)
+
